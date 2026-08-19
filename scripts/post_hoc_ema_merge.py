3409f0ed1c3e31b82e4d7293916395fe8e91ce48
hadxp <hadxpk@gmail.com>
2026-08-19 21:42:47 +0200

    added a dynamic epoch limit to the ema merge script

--- scripts/post_hoc_ema_merge.py
+++ scripts/post_hoc_ema_merge.py
@@ -1,6 +1,9 @@
 import argparse
 from pathlib import Path
+from typing import List
+
 from lora_post_hoc_ema import merge_lora_weights_with_post_hoc_ema
+
 
 def setup_argparse() -> argparse.ArgumentParser:
     """Set up command line argument parsing."""
@@ -19,7 +22,7 @@ def setup_argparse() -> argparse.ArgumentParser:
     parser.add_argument(
         "--sigmarel",
         type=float,
-        default=0.15,
+        default=0.25,
         help="The value for sigma_rel",
     )
     parser.add_argument(
@@ -28,6 +31,23 @@ def setup_argparse() -> argparse.ArgumentParser:
         default=None,
     )
     return parser
+
+
+def _get_files_for_limit(limits: List[int], safetensor_files: List[Path]) -> List[Path]:
+    files = []
+    for epoch_limit in limits:
+        for file in safetensor_files:
+            epoch_str = str(epoch_limit)
+            if ((epoch_str in file.stem) or (epoch_str in file.parent.stem)
+                and (
+                    ".safetensors" in file.suffix
+                    or ".pkl" in file.suffix
+                    or ".pt" in file.suffix
+                )
+            ):
+                files.append(file)
+    return files
+
 
 def main():
     # Parse command line arguments
@@ -43,7 +63,7 @@ def main():
     output_file: str = str(lora_main_dir_path / "merge.safetensors")
 
     # Collect all safetensor files in a flat list
-    safetensor_files = []
+    safetensor_files: List[Path] = []
 
     # Add files from main directory
     safetensor_files.extend(lora_main_dir_path.glob("*.safetensors"))
@@ -53,38 +73,49 @@ def main():
         if folder.is_dir():
             safetensor_files.extend(folder.glob("*.safetensors"))
 
+    if limit:
+        if "-" in limit:
+            limits = limit.split("-")
+            if len(limits) < 2:
+                raise ValueError("At least two limit are required")
+            limit_min = int(limits[0])  # min
+            limit_max = int(limits[1])  # max
+            limits = []
+            # add 1 to limit_min until limit_max is reached
+            while limit_min <= limit_max:
+                limits.append(int(limit_min))
+                limit_min += 1
+            safetensor_files = _get_files_for_limit(limits, safetensor_files)
+
+        if "," in limit:  # separator
+            limits = limit.split(",")
+            safetensor_files = _get_files_for_limit(limits, safetensor_files)
+
+    # Convert to strings
+    files = [str(p) for p in safetensor_files]
+
     # exclude all previous merges
     safetensor_files = [p for p in safetensor_files if "merge" not in p.stem]
 
     # exclude model.safetensorns (if present)
     safetensor_files = [p for p in safetensor_files if "model" not in p.stem]
 
-    # exclude any orig safetenso files
+    # exclude any orig safetensor files
     safetensor_files = [p for p in safetensor_files if "orig" not in p.stem]
-    
+
     if no_const:
-        safetensor_files = [p for p in safetensor_files
-                            if "const" not in p.stem and "constant" not in p.stem]
-    
-    if limit:
-        if "," in limit: # separator
-            limitsplit = limit.split(",")
-            files = []
-            for epoch_limit in limitsplit:
-                for file in safetensor_files:
-                    if epoch_limit in file.stem:
-                        files.append(file)
-            safetensor_files = files
-                        
-    # Convert to strings
-    files = [str(p) for p in safetensor_files]
-    
+        safetensor_files = [
+            p
+            for p in safetensor_files
+            if "const" not in p.stem and "constant" not in p.stem
+        ]
+
     filess = [p.stem for p in safetensor_files]
-    sorted_filess = sorted(filess, key=lambda p: p)
+    #sorted_filess = sorted(filess, key=lambda p: p)
     #print(str(sorted_filess))
-    
-    merge_lora_weights_with_post_hoc_ema(files, False, 0.30, None, sigmarel, output_file)
-    
-    
+
+    merge_lora_weights_with_post_hoc_ema(files, False, None, None, sigmarel, output_file)
+
+
 if __name__ == "__main__":
     main()
